3fe010c1ef444ec25bdecd929cf2bac4f0fc57f8
dason007 <15764240573@163.com>
2018-11-20 09:18:52 +0800

    用户中心

--- app/src/main/java/com/example/com/morecharge/usercenter/MineActivity.java
+++ app/src/main/java/com/example/com/morecharge/usercenter/MineActivity.java
@@ -2,15 +2,24 @@ package com.example.com.morecharge.usercenter;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.widget.RadioButton;
 
 import com.example.com.common.BaseActivity;
 import com.example.com.morecharge.R;
+
+import butterknife.BindView;
 
 /**
  * 我的  activity
  */
 
 public class MineActivity extends BaseActivity {
+
+    @BindView(R.id.rb_receive)
+    RadioButton mRbReceive;
+    @BindView(R.id.rb_release)
+    RadioButton mRbRelease;
+
     @Override
     public int bindLayout() {
         return R.layout.activity_mine;
